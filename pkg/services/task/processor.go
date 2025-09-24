e9d799f97d4003563bbe18469a28b03a8af04dcb
Sergey Antonov <lenktongm@gmail.com>
2025-09-24 22:27:15 +0300

    feat: add file downloading

--- pkg/services/task/processor.go
+++ pkg/services/task/processor.go
@@ -1,5 +1,15 @@
 package task
 
+import (
+	"downloader/pkg/fileutils"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
 type processor struct {
 }
 
@@ -10,7 +20,41 @@ func newProcessor() *processor {
 // TODO: wait for goroutines to finish
 func (p *processor) Start(t *task) {
 	t.status = started
-	go func() {
-		t.status = finished
-	}()
+	go downloadLinks(t)
+}
+
+func downloadLinks(t *task) {
+	taskDir := filepath.Join(DownloadDir, strconv.Itoa(t.id))
+	fileutils.EnsureDir(taskDir)
+
+	for i, link := range t.links {
+		fp := filepath.Join(taskDir, strconv.Itoa(i))
+		err := downloadLink(link, fp)
+		if err != nil {
+			log.Printf("ERROR: downloading file #%v from task %v: %v\n", i, t.id, err)
+		}
+	}
+	t.status = finished
+}
+
+func downloadLink(link string, filepath string) error {
+	// NOTE: maybe we should create the file before downloading it
+	// TODO: check the response code (what if it is 404?)
+	resp, err := http.Get(link)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+	log.Printf("INFO: file %v downloaded successfully\n", filepath)
+	return nil
 }
